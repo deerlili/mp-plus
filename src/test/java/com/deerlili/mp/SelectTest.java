44a8b6a0766b04b5b6093f57355f718b17c989c4
deerlili <deerlili@163.com>
2021-01-16 16:41:50 +0800

    添加mp-select测试代码

--- src/test/java/com/deerlili/mp/SelectTest.java
+++ src/test/java/com/deerlili/mp/SelectTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -45,7 +45,8 @@ public class SelectTest {
     @Test
     public void selectByMap() {
         Map<String,Object> columnMap = new HashMap<>();
-        /**
+
+        /*
          * 键是数据库中的列
          */
         columnMap.put("age",30);
@@ -56,14 +57,14 @@ public class SelectTest {
 
     @Test
     public void selectByWrapper1() {
-        /**
+        /*
          * 有三种方式：
          *      QueryWrapper<User> userWrapper = new QueryWrapper<User>();
          *      QueryWrapper<User> query = Wrappers.<User>query();
          *
          */
-        QueryWrapper<User> userWrapper = new QueryWrapper<User>();
-        /**
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
          * @column 数据库中的列名
          * 查询 name like '%器%' and age < 32
          */
@@ -74,8 +75,8 @@ public class SelectTest {
 
     @Test
     public void selectByWrapper2() {
-        QueryWrapper<User> userWrapper = new QueryWrapper<User>();
-        /**
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
          * 查询 name like '%器%' and age between 20 and 40 and email is not null
          */
         userWrapper.like("name","器")
@@ -87,8 +88,8 @@ public class SelectTest {
 
     @Test
     public void selectByWrapper3() {
-        QueryWrapper<User> userWrapper = new QueryWrapper<User>();
-        /**
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
          * 查询
          * name like '小%' or age >= 20 order by age desc,id asc
          */
@@ -99,5 +100,138 @@ public class SelectTest {
                 .orderByAsc("id");
         List<User> userList = userMapper.selectList(userWrapper);
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper4() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * date_format(create_time,'%Y-%m-%d') and manager_id in
+         * (select id from mp_user where name like '%碗%')
+         * # 有sql注入的风险
+         * apply("date_format(create_time,'%Y-%m-%d') = '2020-08-12'")
+         * # 建议使用第二种
+         * apply("date_format(create_time,'%Y-%m-%d') = {0}", "2020-08-12")
+         */
+        userWrapper
+                .apply("date_format(create_time,'%Y-%m-%d') = '2020-08-12' or true")
+                .inSql("manager_id","select id from mp_user where name like '%碗%'");
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper5() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * name like '小%' and (age < 40 or email is not null)
+         * 使用 Lambda 表达式
+         */
+        userWrapper.likeRight("name","小")
+                .and(wq -> wq
+                        .lt("age",40)
+                        .or().isNotNull("email"));
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper6() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * name like '小%' or (age < 28 and age > 40 and email is not null)
+         * 使用lambda表达式
+         */
+        userWrapper.likeRight("name","小")
+                .or(wq -> wq
+                        .lt("age",28)
+                        .gt("age",40)
+                        .isNotNull("email"));
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper7() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * (age < 40 or email is not null) and name like '小%'
+         * 使用lambda表达式
+         */
+        userWrapper.nested(wq -> wq.lt("age",40).or().isNotNull("email"))
+                .likeRight("name","小");
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper8() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * age in (25,30,40)
+         */
+        userWrapper.in("age",Arrays.asList(25,30,40));
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapper9() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * limit 1
+         * 备注：有SQL注入风险，不是重要表可以使用
+         */
+        userWrapper.last("limit 1");
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapperSupper1() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * 查询满足条件 name like '%器%' and age < 32 的
+         * id 和 name 字段
+         */
+        userWrapper.select("id","name")
+                .like("name","器").lt("age",32);
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByWrapperSupper2() {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * 查询满足条件 name like '%器%' and age < 32 的
+         * 除了 id 和 name 字段
+         */
+        userWrapper.like("name","器").lt("age",32)
+                .select(User.class, info -> !info.getColumn().equals("id")
+                        && !info.getColumn().equals("name"));
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    private void selectByWrapperSupper3Condition(String name) {
+        QueryWrapper<User> userWrapper = new QueryWrapper<>();
+        /*
+         * 以前的写法
+         */
+        if (!StringUtils.isEmpty(name)) {
+            userWrapper.eq("name",name);
+        }
+        /*
+         * 现在的写法
+         */
+        userWrapper.eq(!StringUtils.isEmpty(name),"name",name);
+        List<User> userList = userMapper.selectList(userWrapper);
+        userList.forEach(System.out::println);
+    }
+    @Test
+    public void selectByWrapperSupper3() {
+        String name = "";
+        selectByWrapperSupper3Condition(name);
     }
 }
